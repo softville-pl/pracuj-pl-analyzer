3bfa9852db5260f1233b319e81e2b751f7bcf914
Romaniec Jakub <jakub.romaniec@outlook.com>
2024-08-21 07:15:10 +0200

    Save polish diacritic signs without escaping

--- src/Softville.Analyzer.PracujPl/StringExtensions.cs
+++ src/Softville.Analyzer.PracujPl/StringExtensions.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace ConsoleApp;
@@ -14,6 +15,7 @@ internal static class StringExtensions
 
     public static string JsonPrettify(this JsonElement jElem)
     {
-        return JsonSerializer.Serialize(jElem, new JsonSerializerOptions {WriteIndented = true});
+        return JsonSerializer.Serialize(jElem,
+            new JsonSerializerOptions {WriteIndented = true, Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping});
     }
 }
